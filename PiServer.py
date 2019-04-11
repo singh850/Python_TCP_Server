f409a3b4c3b67fc50aec85cfd6cc6b70a6a03343
singh850 <49505990+singh850@users.noreply.github.com>
2019-04-11 09:32:13 +0530

    Add files via upload

---
+++ PiServer.py
@@ -0,0 +1,56 @@
+import lighton
+import projectHalf2
+from socket import *
+from time import ctime
+import time
+
+lighton.setUP()
+
+ctrCmd = ['UP','DOWN','VIEW']
+message="hello world"
+HOST = '192.168.43.96'
+PORT = 5555
+
+BUFSIZE = 1024
+ADDR = (HOST,PORT)
+
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+
+        
+while True:
+        tcpCliSock,addr = tcpSerSock.accept()
+        
+        time.sleep(.05)
+        data = ''
+        data = tcpCliSock.recv(BUFSIZE).decode()
+        
+        
+        print(data)
+        
+        if not data:
+                print("there is no data")
+                break
+        
+        if data == ctrCmd[0]:
+        	lighton.ledOn()
+        
+        if data == ctrCmd[1]:
+                lighton.ledOff()
+        
+        if data == ctrCmd[2]:
+                lighton.ledOn()
+                #here I want to send String back to Android 
+
+        	
+
+        
+        	
+                
+tcpCliSock.close();
+
+
+
+
+
